5c0cce8d2bf5b28511f4f0f2dd9ec01499729b91
Bardoll <t.labroue@gmail.com>
2014-03-17 20:10:16 +0100

    Ajout des fonctions initialGrille et afficherGrille (non testées)

--- main.c
+++ main.c
@@ -3,5 +3,36 @@
 #include <time.h>
 
 int main(void){
-	char grille[10][10]; //plateau de jeu
+
+	/* VARIABLES *****************************************/
+		char grille[10][10]; //plateau de jeu
+
+	/* CODE **********************************************/
+
+	do{
+		bonjour();
+		initialGrille(grille); //on initialise le plateau
+		afficherGrille(grille); //on affiche le plateau
+	}while(jeu==1);
+	
+}
+
+
+void initialGrille(char grille[x][y]){
+	for(int i=0; i<9; i++){
+		for(int j=0; j<9; j++){
+				grille[i][j]="~";
+		}
+	}
+}
+
+
+
+char afficherGrille(char grille[x][y]){
+	for(int i=0; i<9; i++){
+		for(int j=0; j<9; j++){
+			printf("%s", &grille[i][j]);
+		}
+		printf("\n");
+	}
 }
