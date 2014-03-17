7f2924b2a22ba1752956686af23477728cbd152c
Bardoll <t.labroue@gmail.com>
2014-03-17 21:42:41 +0100

    Le C, ça pue

--- main.c
+++ main.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
+int i,j=0;
+
+void bonjour(){
+	printf("Bienvenue dans le jeu Bataille navale !\n\n");
+	printf("Le but de ce jeu est de détruire tous les bateaux de l'adversaire en envoyant des missiles à chaque tour\n");
+	printf("Pour cela, vous disposez d'un plan de la zone où se cache votre ennemi\n");
+	printf("A chaque tour, entrez les coordonnées d'un point et un missile sera envoyé !\n");
+	printf("Bon courage soldat !\n\n\n");
+}
+
+void initialGrille(char grille[i][j]){
+	for(int i=0; i<9; i++){
+		for(int j=0; j<9; j++){
+				grille[i][j]="~";
+		}
+	}
+}
+
+
+
+void afficherGrille(char grille[i][j]){
+	for(int i=0; i<9; i++){
+		for(int j=0; j<9; j++){
+			printf("%s", &grille[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
 int main(void){
 
 	/* VARIABLES *****************************************/
@@ -15,32 +45,4 @@ int main(void){
 		afficherGrille(grille); //on affiche le plateau
 	}while(jeu==1);
 	
-}
-
-
-void bonjour(){
-	printf("Bienvenue dans le jeu Bataille navale !\n\n");
-	printf("Le but de ce jeu est de détruire tous les bateaux de l'adversaire en envoyant des missiles à chaque tour\n");
-	printf("Pour cela, vous disposez d'un plan de la zone où se cache votre ennemi\n");
-	printf("A chaque tour, entrez les coordonnées d'un point et un missile sera envoyé !\n");
-	printf("Bon courage soldat !\n\n\n");
-}
-
-void initialGrille(char grille[x][y]){
-	for(int i=0; i<9; i++){
-		for(int j=0; j<9; j++){
-				grille[i][j]="~";
-		}
-	}
-}
-
-
-
-void afficherGrille(char grille[x][y]){
-	for(int i=0; i<9; i++){
-		for(int j=0; j<9; j++){
-			printf("%s", &grille[i][j]);
-		}
-		printf("\n");
-	}
 }
